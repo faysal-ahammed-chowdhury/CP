a9e043990f374d59885e93c01c015f423279b146
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-07-09 09:37:36 +0600

    added: stress testing

--- Submissions/Others/Codes/brute.cpp
+++ Submissions/Others/Codes/brute.cpp
@@ -1,38 +1,356 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int power(int x, long long n, int mod) {
-  int ans = 1 % mod;
-  while (n > 0) {
-    if (n & 1) {
-      ans = 1LL * ans * x % mod;
-    }
-    x = 1LL * x * x % mod;
-    n >>= 1;
-  }
-  return ans;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0)
+
+#define PB push_back
+
+#define SZ 200005
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define endl '\n'
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pil> vpil;
+typedef vector<pli> vpli;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<bool> vbl;
+typedef vector<string> vst;
+
+// ordered_set
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+// template<class T> using ordered_set =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// template<class T> using ordered_multiset =tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+/* debugger begin */
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '"' << x << '"';}
+void __print(const string &x) {cerr << '"' << x << '"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T, typename V, typename Z>
+void __print(const tuple<T, V, Z> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+void err(istream_iterator<string> it) {
+    cerr << '\n';
+}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << "=";
+    __print(a);
+    cerr << ' ';
+    err(++it, args...);
 }
 
-int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+/* debugger end */
 
-  int n, m; cin >> n >> m;
-  int a[n + 1];
-  for (int i = 1; i <= n; i++) {
-    cin >> a[i];
-  }
-  sort(a + 1, a + n + 1);
+//const ll MOD = 1000000007;
 
-  int mx = 0;
-  do {
-    int ans = 0;
-    for (int i = 1; i <= n; i++) {
-      ans += power(a[i], i, m);
+const ll INF = (1LL<<59);
+
+// point update
+// range query
+struct Segtree {
+    vector<pll> values;
+    int n;
+
+    Segtree() {}
+
+    Segtree(int n) {
+        init(n);
     }
-    mx = max(mx, ans);
-  } while (next_permutation(a + 1, a + n + 1));
-  cout << mx << '\n';
 
-  return 0;
+    void init(int n) {
+        this->n = n;
+        values.resize(4*n);
+    }
+
+    pll merge(pll a, pll b) {
+        if (a.first == b.first) {
+            return make_pair(a.first, a.second+b.second);
+        }
+        else {
+            return max(a, b);
+        }
+    }
+
+    void build(vector<ll> &a, int at, int lo, int hi) {
+        if (lo == hi) {
+            values[at] = {a[lo], 1LL};
+            return;
+        }
+        int mid = (lo+hi)/2;
+        build(a, at*2, lo, mid);
+        build(a, at*2+1, mid+1, hi);
+        values[at] = merge(values[at*2], values[at*2+1]);
+    }
+
+    void build(vector<ll>&a) {
+        build(a, 1, 1, n);
+    }
+
+    // point update
+    // assign val to a[i]
+    void update(int at, int lo, int hi, int i, ll val, int add) {
+        if (i < lo || hi < i) return;
+        if (lo == hi) {
+            if (add) {
+                values[at].first += val;
+            }
+            else {
+                values[at].first = val;
+            }
+            return;
+        }
+        int mid = (lo+hi)/2;
+        update(at*2, lo, mid, i, val, add);
+        update(at*2+1, mid+1, hi, i, val, add);
+        values[at] = merge(values[at*2], values[at*2+1]);
+    }
+
+    void update(int i, ll val, int add) {
+        update(1, 1, n, i, val, add);
+    }
+
+    void update(int at, int lo, int hi, int i, pll val) {
+        if (i < lo || hi < i) return;
+        if (lo == hi) {
+            values[at] = val;
+            return;
+        }
+        int mid = (lo+hi)/2;
+        update(at*2, lo, mid, i, val);
+        update(at*2+1, mid+1, hi, i, val);
+        values[at] = merge(values[at*2], values[at*2+1]);
+    }
+
+    void update(int i, pll val) {
+        update(1, 1, n, i, val);
+    }
+
+    pll query() {
+        return values[1];
+    }
+};
+
+// point update
+// range query
+struct Segtree2 {
+    vector<pll> values;
+    int n;
+
+    Segtree2() {}
+
+    Segtree2(int n) {
+        init(n);
+    }
+
+    void init(int n) {
+        this->n = n;
+        values.resize(4*n);
+    }
+
+    pll merge(pll a, pll b) {
+        return max(a, b);
+    }
+
+    void build(vector<pll> &a, int at, int lo, int hi) {
+        if (lo == hi) {
+            values[at] = a[lo];
+            return;
+        }
+        int mid = (lo+hi)/2;
+        build(a, at*2, lo, mid);
+        build(a, at*2+1, mid+1, hi);
+        values[at] = merge(values[at*2], values[at*2+1]);
+    }
+
+    void build(vector<pll>&a) {
+        build(a, 1, 1, n);
+    }
+
+    // point update
+    // assign val to a[i]
+    void update(int at, int lo, int hi, int i, ll val, int add) {
+        if (i < lo || hi < i) return;
+        if (lo == hi) {
+            if (add) {
+                values[at].first += val;
+            }
+            else {
+                values[at].first = val;
+            }
+            return;
+        }
+        int mid = (lo+hi)/2;
+        update(at*2, lo, mid, i, val, add);
+        update(at*2+1, mid+1, hi, i, val, add);
+        values[at] = merge(values[at*2], values[at*2+1]);
+    }
+
+    void update(int i, ll val, int add) {
+        update(1, 1, n, i, val, add);
+    }
+
+    void update(int at, int lo, int hi, int i, pll val) {
+        if (i < lo || hi < i) return;
+        if (lo == hi) {
+            values[at] = val;
+            return;
+        }
+        int mid = (lo+hi)/2;
+        update(at*2, lo, mid, i, val);
+        update(at*2+1, mid+1, hi, i, val);
+        values[at] = merge(values[at*2], values[at*2+1]);
+    }
+
+    void update(int i, pll val) {
+        update(1, 1, n, i, val);
+    }
+
+    pll query() {
+        return values[1];
+    }
+};
+
+void dfs1(int u, vector<vi> &G, vi &order, vi &visited) {
+    visited[u] = 1;
+    for (auto v : G[u]) {
+        if (!visited[v]) {
+            dfs1(v, G, order, visited);
+        }
+    }
+    order.PB(u);
+}
+ 
+void dfs2(int u, vector<vi> &Gr, vi &comp, vi &visited) {
+    visited[u] = 1;
+    comp.PB(u);
+    for (auto v : Gr[u]) {
+        if (!visited[v]) {
+            dfs2(v, Gr, comp, visited);
+        }
+    }
+}
+
+void solve(int tt) {
+    int n, m, q;
+    cin >> n >> m >> q;
+    vvi G(n+1), Gr(n+1);
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        G[u].PB(v);
+        Gr[v].PB(u);
+    }
+    vi s(n+1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> s[i];
+    }
+    vi order;
+    vi vis(n+1), vis2(n+1);
+    for (int i = 1; i <= n; ++i) {
+        if (!vis[i]) {
+            dfs1(i, G, order, vis);
+        }
+    }
+    // error(order);
+    int cnt = 0;
+    vpii a(n+1);
+    vector<Segtree> ss;
+    reverse(all(order));
+    for (auto u : order) {
+        if (!vis2[u]) {
+            vi comp;
+            dfs2(u, Gr, comp, vis2);
+            // error(comp);
+            int sz = comp.size();
+            vll b(sz+1);
+            for (int j = 0; j < sz; ++j) {
+                int v = comp[j];
+                a[v] = {cnt, j+1};
+                b[j+1] = s[v];
+            }
+            Segtree st(sz);
+            st.build(b);
+            ss.emplace_back(st);
+            ++cnt;
+        }
+    }
+    Segtree2 st(cnt);
+    vpll d(cnt+1);
+    for (int i = 1; i <= cnt; ++i) {
+        d[i] = ss[i-1].query();
+    }
+    vll c(cnt);
+    st.build(d);
+    while (q--) {
+        string t;
+        cin >> t;
+        if (t == "AC") {
+            ll x, y;
+            cin >> x >> y;
+            auto [u, v] = a[y];
+            st.update(u+1, x, 1);
+            c[u] += x;
+        }
+        else if (t == "AS") {
+            ll x, y;
+            cin >> x >> y;
+            auto [u, v] = a[y];
+            ss[u].update(v, x, 1);
+            pll ret = ss[u].query();
+            ret.first += c[u];
+            st.update(u+1, ret);
+        }
+        else {
+            auto [u, v] = st.query();
+            if (u < 0) cout << 0 << endl;
+            else cout << u << endl;
+        }
+    }
+    // error(G, Gr);
+}
+
+int main() {
+    FASTIO;
+    int tc = 1;
+    // cin >> tc;
+    for (int tt = 1; tt <= tc; tt++)
+    {
+        solve(tt);
+    }
+    return 0;
 }
--- Submissions/Others/Codes/code.cpp
+++ Submissions/Others/Codes/code.cpp
@@ -1,41 +1,176 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+// #define int long long
 
-int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+#ifdef SHAMIM_AHMED
+    #include <G:\Desktop Items\STATION\CPP\debug.h>
+#else
+    #define debug(x...) 
+    #define debug2(x...) 
+    #define debug3(x...)
+#endif
 
-  int n, m; cin >> n >> m;
-  int zero = 0, one = 0, two = 0, three = 0;
-  for (int i = 1; i <= n; i++) {
-    int x; cin >> x;
-    zero += x == 0;
-    one += x == 1;
-    two += x == 2;
-    three += x == 3;
-  }
+const int N = 2e5+10;
+vector<int> adj[2][N];
+bool vis[2][N];
+stack<int> stk;
+int c = 0;
+int j = 1;
+map<int,int> comp;
+map<int,int> idx;
+map<int,int> comp_size;
+map<int,int> comp_start;
+int a[N];
+void dfs(int vertex,int t)
+{
+    if(t == 1)
+    {
+        comp_size[c]++;
+        comp[vertex] = c;
+        idx[vertex] = j++;
+    }
 
-  if (m == 2) {
-    int ans = one;
-    cout << ans << '\n';
-  }
-  else if (m == 3) {
-    int ans = one;
-    int possilbe = (n + 1) / 2;
-    ans += min(possilbe, two) * 2;
-    two -= min(possilbe, two);
-    ans += two;
-    cout << ans << '\n';
-  }
-  else {
-    int ans = one;
-    int possilbe = (n + 1) / 2;
-    if (three < possilbe and two) ans += 2;
-    ans += min(possilbe, three) * 3;
-    three -= min(possilbe, three);
-    ans += three;
-    cout << ans << '\n';
-  }
+    vis[t][vertex] = true;
+    for(auto child: adj[t][vertex])
+    {
+        if(vis[t][child]) continue;
 
-  return 0;
+        dfs(child,t);
+    }
+    if(t == 0)
+    stk.push(vertex);
+}
+
+
+struct ST {
+    int tree[4 * N], lazy[4 * N];
+    void push(int n, int b, int e) {
+        if (lazy[n] == 0) return;
+        tree[n] += lazy[n];
+        if (b != e) {
+            int l = n << 1, r = l + 1;
+            lazy[l] += lazy[n];
+            lazy[r] += lazy[n];
+        }
+        lazy[n] = 0;
+    }
+    void build(int n, int b, int e) {
+        lazy[n] = 0;
+        if (b == e) {
+            tree[n] = a[b];
+            return;
+        }
+        int mid = (b + e) >> 1;
+        build(n * 2, b, mid);
+        build(n * 2 + 1, mid + 1, e);
+        tree[n] = max(tree[n * 2], tree[n * 2 + 1]);
+    }
+    void upd(int n, int b, int e, int i, int j, int x) {
+        push(n, b, e);
+        if (e < i or b > j) {
+            return;
+        }
+        if (b >= i and e <= j) {
+            lazy[n] += x;
+            push(n, b, e);
+                return;
+        }
+        int mid = (b + e) >> 1;
+        upd(n * 2,b, mid,i, j, x);
+        upd(n * 2 + 1,mid + 1,e,i, j, x);
+        tree[n] = max(tree[n * 2], tree[n * 2 + 1]);
+    }
+    int query(int n, int b, int e, int i, int j) {
+        push(n, b, e);
+        if (e < i or b > j) {
+            return 0;
+        }
+        if (b >= i and e <= j) {
+                return tree[n];
+        }
+        int mid = (b + e) >> 1;
+        int L = query(n * 2,b, mid,i, j);
+        int R = query(n * 2 + 1,mid + 1,e,i, j);
+        return max(L, R);
+    }
+} st;
+
+void solve()
+{
+    int n,m,q;
+    cin >> n >> m >> q;
+
+    for(int i = 0; i < m; i++)
+    {
+        int u,v;
+        cin >> u >> v;
+        adj[0][u].push_back(v);
+        adj[1][v].push_back(u);
+    }
+
+    int income[n + 1];
+    for (int i = 1; i <= n; i++) {
+        cin >> income[i];
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(vis[0][i] == false)
+        {
+            dfs(i,0);
+        }
+    }
+    j = 1;
+    while(stk.size())
+    {
+        if(vis[1][stk.top()] == false)
+        {
+            c++;
+            comp_start[c] = j;
+            dfs(stk.top(),1);
+        }
+        stk.pop();
+    }
+
+    // for(auto [x,y]: comp)
+    // {
+    //     cout << x << " -> " << idx[x] << endl;
+    //     // cout << idx[x] << endl;
+    // }    
+
+    // debug(comp_size);
+    // debug(comp_start);
+    for (int i = 1; i <= n; i++) {
+        a[idx[i]] = income[i];
+    }
+    st.build(1, 1, n);
+    while (q--) {
+        string Q; cin >> Q;
+        if (Q=="Q") {
+            cout << max(st.query(1,1, n, 1, n), 0) << '\n';
+        }
+        else if (Q == "AC") {
+            int x, y; cin >> x >> y;
+            int start = comp_start[comp[y]];
+            int end = start + comp_size[comp[y]] - 1;
+            st.upd(1, 1, n, start, end, x);
+        }
+        else {
+            int x, y; cin >> x >> y;
+            st.upd(1, 1, n, idx[y], idx[y], x);
+        }
+    }
+    
+
+
+}
+
+int32_t main()
+{
+    //    Bismillah
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
 }
