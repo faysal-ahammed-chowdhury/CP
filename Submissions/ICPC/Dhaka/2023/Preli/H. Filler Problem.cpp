bd5bd3e51833baadb386a4cd8cb42466a05a5e7c
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-07-30 12:45:36 +0600

    Added dp optimization using bitset problem

--- Submissions/ICPC/Dhaka/2023/Preli/H. Filler Problem.cpp
+++ Submissions/ICPC/Dhaka/2023/Preli/H. Filler Problem.cpp
@@ -3,37 +3,54 @@ using namespace std;
 const int N = 125;
 int n, a[N];
 long long prefix[N];
-bitset<5005> vis[N][5005];
+// bitset<5005> vis[N][5005];
 bitset<5005> dp[N][5005];
 
-bool fun(int i, int sum1, int sum2) {
-  if (i == 0) {
-    if (sum1 == 0 and sum2 == 0) {
-      return true;
+// bool f(int i, int sum1, int sum2) {
+//   if (i == 0) {
+//     if (sum1 == 0 and sum2 == 0) {
+//       return true;
+//     }
+//     return false;
+//   }
+//   if (vis[i][sum1][sum2]) return dp[i][sum1][sum2];
+//   bool ans = f(i - 1, sum1, sum2);
+//   if (ans) {
+//     dp[i][sum1][sum2] = 1;
+//     return ans;
+//   }
+//   if (sum1 - a[i] >= 0) {
+//     ans = f(i - 1, sum1 - a[i], sum2);
+//     if (ans) {
+//       dp[i][sum1][sum2] = 1;
+//       return ans;
+//     }
+//   }
+//   if (sum2 - a[i] >= 0) {
+//     ans = f(i - 1, sum1, sum2 - a[i]);
+//     if (ans) {
+//       dp[i][sum1][sum2] = 1;
+//       return ans;
+//     }
+//   }
+//   return false;
+// }
+
+bool f(int to, int sum) { // optimized using bitset
+  for (int i = 1; i <= to; i++) {
+    for (int x = 0; x <= sum; x++) {
+      dp[i][x].reset();
     }
-    return false;
   }
-  if (vis[i][sum1][sum2]) return dp[i][sum1][sum2];
-  bool ans = fun(i - 1, sum1, sum2);
-  if (ans) {
-    dp[i][sum1].set(sum2);
-    return ans;
-  }
-  if (sum1 - a[i] >= 0) {
-    ans = fun(i - 1, sum1 - a[i], sum2);
-    if (ans) {
-      dp[i][sum1].set(sum2);
-      return ans;
+  dp[0][0][0] = 1;
+  for (int i = 1; i <= to; i++) {
+    for (int x = 0; x <= sum; x++) {
+      dp[i][x] = dp[i - 1][x];
+      if (x - a[i] >= 0) dp[i][x] |= dp[i - 1][x - a[i]];
+      dp[i][x] |= dp[i - 1][x] << a[i];
     }
   }
-  if (sum2 - a[i] >= 0) {
-    ans = fun(i - 1, sum1, sum2 - a[i]);
-    if (ans) {
-      dp[i][sum1].set(sum2);
-      return ans;
-    }
-  }
-  return false;
+  return dp[to][sum][sum];
 }
 
 int cs = 0;
@@ -48,7 +65,7 @@ void solve() {
   cout << "Case " << ++cs << ":\n";
   for (int i = 1; i <= n; i++) {
     if (prefix[i] % 3 == 0) {
-      cout << fun(i, prefix[i] / 3, prefix[i] / 3) << '\n';
+      cout << f(i, prefix[i] / 3) << '\n';
     }
     else {
       cout << 0 << '\n';
