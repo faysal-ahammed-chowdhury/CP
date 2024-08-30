97f0feb9b33b553a6430304ffbb720f5e584d12b
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-08-30 15:10:42 +0600

    Added LightOJ Problem [Segment Tree]

--- Submissions/Others/Codes/gen.cpp
+++ Submissions/Others/Codes/gen.cpp
@@ -15,27 +15,12 @@ int32_t main() {
   cin.tie(0);
 
   int n = get_rand(2, N);
-  int m = get_rand(N, N + 50);
-  int q = get_rand(30, 500);
-  cout << n << ' ' << m << ' ' << q  << '\n';
-  while (m--) {
-    cout << get_rand(1, n) << ' ' << get_rand(1, n) << '\n';
-  }
+  cout << 1 << '\n';
+  cout << n << '\n';
   for (int i = 1; i <= n; i++) {
     cout << get_rand(0, 500) << ' ';
   }
   cout << '\n';
 
-  string s[3] = {"Q", "AC", "AS"};
-  while (q--) {
-    int idx = get_rand(0, 2);
-    if (s[idx] == "Q") {
-      cout << "Q\n";
-      continue;
-    }
-    cout << s[idx] << ' ' << get_rand(-500, 500) << ' ' << get_rand(1, n) << '\n';
-  }
-  
-  
   return 0;
 }
