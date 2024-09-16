00a8d4923204efd2613bcd1b7520d5232717529a
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-09-16 14:52:12 +0600

    Update Sparse Table Code

--- Submissions/Others/Codes/Array Stabilization (GCD version).cpp
+++ Submissions/Others/Codes/Array Stabilization (GCD version).cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 2e5 + 9, K = 25; 
-int st[K + 1][N], n, gc;
-vector<int> a;
+const int N = 2e5 + 9, K = 20; 
+int a[N], tree[N][K], n;
 
 int log2_floor(unsigned long long i) {
   return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
 }
 
 void build(int n) {
-  copy(a.begin(), a.end(), st[0]);
-  for (int i = 1; i <= K; i++) {
-    for (int j = 0; j + (1 << i) <= n; j++) {
-      st[i][j] = __gcd(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+  for (int i = 1; i <= n; i++) {
+    tree[i][0] = a[i];
+  }
+  for (int k = 1; k < K; k++) {
+    for (int i = 1; i + (1 << k) - 1 <= n; i++) {
+      tree[i][k] = __gcd(tree[i][k - 1], tree[i + (1 << (k - 1))][k - 1]); 
     }
   }
 }
 
 int query(int l, int r) {
-  int i = log2_floor(r - l + 1);
-  return __gcd(st[i][l], st[i][r - (1 << i) + 1]);
+  int k = log2_floor(r - l + 1);
+  return __gcd(tree[l][k], tree[r - (1 << k) + 1][k]); 
 }
 
-bool ok(int x) {
+bool ok(int x, int gc) {
   for (int i = 1; i <= n; i++) {
     int g = query(i, min(n, i + x));
     if (i + x > n) {
@@ -36,9 +37,7 @@ bool ok(int x) {
 
 void solve() {
   cin >> n;
-  a.resize(n + 1);
-  gc = 0;
-  a[0] = 0;
+  int gc = 0;
   for (int i = 1; i <= n; i++) {
     cin >> a[i];
     gc = __gcd(gc, a[i]);
@@ -47,7 +46,7 @@ void solve() {
   int l = 0, r = n - 1, ans = -1;
   while (l <= r) {
     int mid = (l + r) >> 1;
-    if (ok(mid)) {
+    if (ok(mid, gc)) {
       ans = mid;
       r = mid - 1;
     }
--- Submissions/Others/Codes/Sparse Table.cpp
+++ Submissions/Others/Codes/Sparse Table.cpp
@@ -1,38 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e6 + 9, K = 25; // change here
-int st[K + 1][N], n;
-vector<int> a;
+const int N = 2e5 + 9, K = 20; // change here
+int a[N], tree[N][K];
 
 int log2_floor(unsigned long long i) {
   return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
 }
 
-void build() { // 0 index
-  copy(a.begin(), a.end(), st[0]);
-  for (int i = 1; i <= K; i++) {
-    for (int j = 0; j + (1 << i) <= n; j++) {
-      st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]); //  change here
+void build(int n) {
+  for (int i = 1; i <= n; i++) {
+    tree[i][0] = a[i];
+  }
+  for (int k = 1; k < K; k++) {
+    for (int i = 1; i + (1 << k) - 1 <= n; i++) {
+      tree[i][k] = min(tree[i][k - 1], tree[i + (1 << (k - 1))][k - 1]); // change here
     }
   }
 }
 
-int query(int l, int r) { // 0 index
-  int i = log2_floor(r - l + 1);
-  return min(st[i][l], st[i][r - (1 << i) + 1]); // change here
+int query(int l, int r) {
+  int k = log2_floor(r - l + 1);
+  return min(tree[l][k], tree[r - (1 << k) + 1][k]); // change here
 }
 
 int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> n;
-  a.resize(n);
-  for (auto &x : a) {
-    cin >> x;
+  int n, q; cin >> n >> q;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
   }
-  build();
+  build(n);
+  while (q--) {
+    int l, r; cin >> l >> r;
+    cout << query(l, r) << '\n';
+  }
 
   return 0;
 }
+// https://cses.fi/problemset/task/1647/
