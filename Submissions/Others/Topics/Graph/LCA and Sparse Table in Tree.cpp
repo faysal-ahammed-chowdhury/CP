58a172ac880b76deaddfb50dc22f4cc26e26ffc2
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-09-17 16:53:36 +0600

    UPD: Update Variable Names

--- Submissions/Others/Topics/Graph/LCA and Sparse Table in Tree.cpp
+++ Submissions/Others/Topics/Graph/LCA and Sparse Table in Tree.cpp
@@ -4,19 +4,19 @@ using namespace std;
 bool CHECK(int N, int pos) { return (bool)(N & (1 << pos)); }
 void SET(int &N, int pos) { (N |= (1 << pos)); }
 
-const int N = 1e5 + 9, LOG = 20, inf = 1e9;
+const int N = 1e5 + 9, LOG = 20, inf = 1e9; // change here
 vector<array<int, 2>> g[N];
-int par[N][LOG], tree[N][LOG], tree2[N][LOG], depth[N];
+int par[N][LOG], tree_mx[N][LOG], tree_mn[N][LOG], depth[N];
 
 void dfs(int u, int p, int dis) {
   par[u][0] = p;
-  tree[u][0] = dis;
-  tree2[u][0] = dis;
+  tree_mx[u][0] = dis;
+  tree_mn[u][0] = dis;
   depth[u] = depth[p] + 1;
   for (int i = 1; i < LOG; i++) {
     par[u][i] = par[par[u][i - 1]][i - 1];
-    tree[u][i] = max(tree[u][i - 1], tree[par[u][i - 1]][i - 1]);
-    tree2[u][i] = min(tree2[u][i - 1], tree2[par[u][i - 1]][i - 1]);
+    tree_mx[u][i] = max(tree_mx[u][i - 1], tree_mx[par[u][i - 1]][i - 1]); // change here
+    tree_mn[u][i] = min(tree_mn[u][i - 1], tree_mn[par[u][i - 1]][i - 1]); // change here
   }
   for (auto [v, w] : g[u]) {
     if (v != p) {
@@ -62,14 +62,14 @@ int query_max(int u, int v) {
   int ans = 0;
   for (int i = 0; i < LOG; i++) {
     if (CHECK(d, i)) {
-      ans = max(ans, tree[u][i]);
+      ans = max(ans, tree_mx[u][i]);
       u = par[u][i];
     }
   }
   d = dist(l, v);
   for (int i = 0; i < LOG; i++) {
     if (CHECK(d, i)) {
-      ans = max(ans, tree[v][i]);
+      ans = max(ans, tree_mx[v][i]);
       v = par[v][i];
     }
   }
@@ -82,14 +82,14 @@ int query_min(int u, int v) {
   int ans = inf;
   for (int i = 0; i < LOG; i++) {
     if (CHECK(d, i)) {
-      ans = min(ans, tree2[u][i]);
+      ans = min(ans, tree_mn[u][i]);
       u = par[u][i];
     }
   }
   d = dist(l, v);
   for (int i = 0; i < LOG; i++) {
     if (CHECK(d, i)) {
-      ans = min(ans, tree2[v][i]);
+      ans = min(ans, tree_mn[v][i]);
       v = par[v][i];
     }
   }
@@ -109,7 +109,7 @@ void solve() {
     g[u].push_back({v, w});
     g[v].push_back({u, w});
   }
-  dfs(1, 0, 0);
+  dfs(1, 0, 0); // change here
   cout << "Case " << ++cs << ":\n";
   int q; cin >> q;
   while (q--) {
