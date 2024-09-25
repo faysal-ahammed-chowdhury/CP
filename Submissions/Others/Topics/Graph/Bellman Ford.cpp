43ce33f74c1dc86a3d1d33cebd7dd8b20850d4de
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-09-25 19:49:26 +0600

    UPD: Bellman Ford.cpp

--- Submissions/Others/Topics/Graph/Bellman Ford.cpp
+++ Submissions/Others/Topics/Graph/Bellman Ford.cpp
@@ -7,34 +7,34 @@ const ll inf = 1e18;
 vector<pair<int, ll>> g[N];
 vector<ll> dis(N, inf);
 int n, m;
-bool cycle;
 
-void bellman_ford(int s) {
+bool bellman_ford(int s) {
   dis[s] = 0;
+  bool has_cycle = false;
   for (int i = 1; i <= n; i++) {
     for (int u = 1; u <= n; u++) {
       for (auto [v, w] : g[u]) {
         if (dis[v] > dis[u] + w and dis[u] != inf) {
-          if (i == n) cycle = true;
+          if (i == n) has_cycle = true;
           dis[v] = dis[u] + w;
         }
       }
     }
   }
+  return has_cycle;
 }
 
 int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  
+
   cin >> n >> m;
   while (m--) {
     int u, v, w; cin >> u >> v >> w;
-    g[u].push_back({ v,w });
-    g[v].push_back({ u,w });
+    g[u].push_back({ v, w });
+    g[v].push_back({ u, w });
   }
 
-  cycle = false;
   bellman_ford(1);
 
   for (int u = 1; u <= n; u++) {
