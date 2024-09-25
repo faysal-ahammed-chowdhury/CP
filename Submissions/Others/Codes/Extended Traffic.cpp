0b3dabc0aeaf83aa01069ec56f213005f1145cc7
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-09-25 19:57:34 +0600

    ADDED: Bellman Ford Practice Problems from LightOJ

--- Submissions/Others/Codes/Extended Traffic.cpp
+++ Submissions/Others/Codes/Extended Traffic.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-#define int long long
-const int N = 205, inf = 1e9;
-vector<array<int, 2>> g[N];
-int n, m, dis[N];
+const int N = 205;
+const ll inf = 1e18;
+vector<array<ll, 2>> g[N];
+vector<ll> dis(N, inf), nodes;
+vector<bool> vis(N, false), not_reachable(N, false);
+int n, m, a[N];
 
-void bellman_ford(int src) {
-  dis[src] = 0;
+void bellman_ford(int s) {
+  dis[s] = 0;
   for (int i = 1; i <= n; i++) {
     for (int u = 1; u <= n; u++) {
       for (auto [v, w] : g[u]) {
         if (dis[v] > dis[u] + w and dis[u] != inf) {
+          if (i == n) {
+            nodes.push_back(v);
+          }
           dis[v] = dis[u] + w;
         }
       }
@@ -19,35 +25,53 @@ void bellman_ford(int src) {
   }
 }
 
+void dfs(int u) {
+  not_reachable[u] = true;
+  vis[u] = true;
+  for (auto [v, w] : g[u]) {
+    if (!vis[v]) {
+      dfs(v);
+    }
+  }
+}
 
 int cs;
 void solve() {
   cin >> n;
+
+  nodes.clear();
   for (int i = 1; i <= n; i++) {
     g[i].clear();
     dis[i] = inf;
+    vis[i] = false;
+    not_reachable[i] = false;
   }
 
-  int a[n + 1];
   for (int i = 1; i <= n; i++) {
     cin >> a[i];
   }
+
   cin >> m;
   while (m--) {
     int u, v; cin >> u >> v;
-    if (u == v) continue;
-    int w = 1ll * (a[v] - a[u]) * (a[v] - a[u]) * (a[v] - a[u]);
-    g[u].push_back({v, w});
+    ll w = 1ll * (a[v] - a[u]) * (a[v] - a[u]) * (a[v] - a[u]);
+    g[u].push_back({ v, w });
   }
-  
+
   bellman_ford(1);
-  cout << "Case " << ++cs << ":\n";
+
+  for (auto u : nodes) {
+    if (!vis[u]) {
+      dfs(u);
+    }
+  }
+
   int q; cin >> q;
+  cout << "Case " << ++cs << ":\n";
   while (q--) {
     int v; cin >> v;
-    int d = dis[v];
-    if (d < 3 or d >= inf) cout << "?\n";
-    else cout << d << '\n';
+    if (not_reachable[v] or dis[v] < 3 or dis[v] >= inf) cout << "?\n";
+    else cout << dis[v] << '\n';
   }
 }
 
@@ -55,8 +79,8 @@ int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  int t = 1; cin >> t;
-  while(t--) {
+  int t; cin >> t;
+  while (t--) {
     solve();
   }
 
