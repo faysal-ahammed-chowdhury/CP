fca2173527b5fcb09c441b65f29e2001294abaf1
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-09-29 20:44:53 +0600

    ADDED: Matrix Expo Template and Example Problem (Template Credit: YouKn0wWho)

---
+++ Submissions/Others/Codes/Number Sequence.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int mod = 10000;
+
+struct Mat { // credit: YouKn0wWho
+  int n, m;
+  vector<vector<int>> a;
+  Mat() { }
+  Mat(int _n, int _m) {n = _n; m = _m; a.assign(n, vector<int>(m, 0)); }
+  Mat(vector< vector<int> > v) { n = v.size(); m = n ? v[0].size() : 0; a = v; }
+  inline void make_unit() {
+    assert(n == m);
+    for (int i = 0; i < n; i++)  {
+      for (int j = 0; j < n; j++) a[i][j] = i == j;
+    }
+  }
+  inline Mat operator + (const Mat &b) {
+    assert(n == b.n && m == b.m);
+    Mat ans = Mat(n, m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        ans.a[i][j] = (a[i][j] + b.a[i][j]) % mod;
+      }
+    }
+    return ans;
+  } 
+  inline Mat operator - (const Mat &b) {
+    assert(n == b.n && m == b.m);
+    Mat ans = Mat(n, m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        ans.a[i][j] = (a[i][j] - b.a[i][j] + mod) % mod;
+      }
+    }
+    return ans;
+  }
+  inline Mat operator * (const Mat &b) {
+    assert(m == b.n);
+    Mat ans = Mat(n, b.m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < b.m; j++) {
+        for(int k = 0; k < m; k++) {
+          ans.a[i][j] = (ans.a[i][j] + 1LL * a[i][k] * b.a[k][j] % mod) % mod;
+        }
+      }
+    }
+    return ans;
+  }
+  inline Mat pow(long long k) {
+    assert(n == m);
+    Mat ans(n, n), t = a; ans.make_unit();
+    while (k) {
+      if (k & 1) ans = ans * t;
+      t = t * t;
+      k >>= 1;
+    }
+    return ans;
+  }
+  inline Mat& operator += (const Mat& b) { return *this = (*this) + b; }
+  inline Mat& operator -= (const Mat& b) { return *this = (*this) - b; }
+  inline Mat& operator *= (const Mat& b) { return *this = (*this) * b; }
+  inline bool operator == (const Mat& b) { return a == b.a; }
+  inline bool operator != (const Mat& b) { return a != b.a; }
+};
+
+int cs;
+void solve() {
+  int a, b, n, m; cin >> a >> b >> n >> m;
+  Mat m1(1, 2), m2(2, 2);
+  m1.a[0][0] = b;
+  m1.a[0][1] = a;
+  m2.a[0][0] = 1;
+  m2.a[0][1] = 1;
+  m2.a[1][0] = 1;
+  m2.a[1][1] = 0;
+  Mat m3 = m2.pow(n - 1);
+  Mat ans = m1 * m3;
+  int dig = 1;
+  for (int i = 1; i <= m; i++) {
+    dig *= 10;
+  }
+  cout << "Case " << ++cs << ": " << ans.a[0][0] % dig << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t; cin >> t;
+  while (t--) {
+    solve();
+  }
+  
+  return 0;
+}
