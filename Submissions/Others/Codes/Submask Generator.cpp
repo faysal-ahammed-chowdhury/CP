4aca81ef8d612f914fe5f335dd6b62dd19181148
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-10-19 02:09:25 +0600

    Added: Submask Generator

---
+++ Submissions/Others/Codes/Submask Generator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int mask = 0; // temporary
+  for (int submask = mask; submask; submask = (submask - 1) & mask) {
+    
+  }
+
+  return 0;
+}
