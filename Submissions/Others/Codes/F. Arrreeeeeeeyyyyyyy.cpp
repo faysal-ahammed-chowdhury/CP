3eecc90f281ddce69fd796470464d873a21b7d2b
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-10-25 00:50:28 +0600

    UPD: Better Implementation

--- Submissions/Others/Codes/F. Arrreeeeeeeyyyyyyy.cpp
+++ Submissions/Others/Codes/F. Arrreeeeeeeyyyyyyy.cpp
@@ -6,40 +6,43 @@ const int N = 1e5 + 9;
 int n, m, a[N], sorted[N], sorted_idx[N];
 
 struct ST {
-  int tree[4 * N];
+  pair<int, int> tree[4 * N];
   void build(int n, int b, int e) {
     if (b == e) {
-      tree[n] = 0;
+      tree[n] = {0, 0};
       return;
     }
     int mid = (b + e) >> 1, l = n << 1, r = l + 1;
     build(l, b, mid);
     build(r, mid + 1, e);
-    tree[n] = tree[l] + tree[r];
+    tree[n].first = tree[l].first + tree[r].first;
+    tree[n].second = tree[l].second + tree[r].second;
   }
-  void upd(int n, int b, int e, int i, int x) {
+  void upd(int n, int b, int e, int i, int x1, int x2) {
     if (b > i || e < i) return;
     if (b == e && b == i) {
-      tree[n] = x;
+      tree[n].first = x1;
+      tree[n].second = x2;
       return;
     }
     int mid = (b + e) >> 1, l = n << 1, r = l + 1;
-    upd(l, b, mid, i, x);
-    upd(r, mid + 1, e, i, x);
-    tree[n] = tree[l] + tree[r];
+    upd(l, b, mid, i, x1, x2);
+    upd(r, mid + 1, e, i, x1, x2);
+    tree[n].first = tree[l].first + tree[r].first;
+    tree[n].second = tree[l].second + tree[r].second;
   }
-  int query(int n, int b, int e, int i, int j) {
-    if (b > j || e < i) return 0;
+  pair<int, int> query(int n, int b, int e, int i, int j) {
+    if (b > j || e < i) return {0, 0};
     if (b >= i && e <= j) return tree[n];
     int mid = (b + e) >> 1, l = n << 1, r = l + 1;
-    int L = query(l, b, mid, i, j);
-    int R = query(r, mid + 1, e, i, j);
-    return (L + R);
+    pair<int, int> L = query(l, b, mid, i, j);
+    pair<int, int> R = query(r, mid + 1, e, i, j);
+    return {(L.first + R.first), (L.second + R.second)};
   }
-} st, st2;
+} st;
 
 bool ok(int len, int max_possible) {
-  return st.query(1, 1, n, 1, len) <= max_possible;
+  return st.query(1, 1, n, 1, len).first <= max_possible;
 }
 
 int32_t main() {
@@ -59,7 +62,6 @@ int32_t main() {
   }
 
   st.build(1, 1, n);
-  st2.build(1, 1, n);
 
   for (int i = 1; i <= n; i++) {
     if (a[i] > m) {
@@ -78,10 +80,9 @@ int32_t main() {
           r = mid - 1;
         }
       }
-      cout << st2.query(1, 1, n, 1, ans) + 1 << ' ';
+      cout << st.query(1, 1, n, 1, ans).second + 1 << ' ';
     }
-    st.upd(1, 1, n, sorted_idx[i], a[i]);
-    st2.upd(1, 1, n, sorted_idx[i], 1);
+    st.upd(1, 1, n, sorted_idx[i], a[i], 1);
   }
   cout << '\n';
 
