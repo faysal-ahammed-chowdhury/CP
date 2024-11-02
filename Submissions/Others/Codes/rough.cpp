2b30860b3ec91a29535a6e437464c21b52584dbc
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-11-03 02:10:36 +0600

    Added: Ubuntu

--- Submissions/Others/Codes/rough.cpp
+++ Submissions/Others/Codes/rough.cpp
@@ -5,15 +5,7 @@ int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  int idx, k; cin >> idx >> k;
-  int mod = (1 << idx);
-  int xr = 0;
-  for (int i = 0; i <= 28; i++) {
-    if ((i % mod) == k) {
-      xr ^= i;
-      cout << xr << '\n';
-    }
-  }
+  
 
   return 0;
 }
