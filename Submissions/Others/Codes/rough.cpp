154c74bebf799656fb42cb56f7887c07d56c1b9f
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2025-01-20 01:09:54 +0600

    changed

--- Submissions/Others/Codes/rough.cpp
+++ Submissions/Others/Codes/rough.cpp
@@ -5,23 +5,7 @@ int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  int n, k; cin >> n >> k;
-  int a[n + 1];
-  for (int i =1; i <= n; i++) {
-    cin >> a[i];
-  }
-
-  int ans = 0;
-  for (int i = 1; i + k - 1 <= n; i++) {
-    int mn = a[i];
-    for (int j = i; j <= i + k - 1; j++) {
-      mn = min(mn, a[j]);
-    }
-    // cout << mn << ' ';
-    ans += mn;
-  }
-
-  cout << ans << '\n';
+  cout << "Hello\n";
 
   return 0;
 }
