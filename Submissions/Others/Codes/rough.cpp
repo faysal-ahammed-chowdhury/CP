50b3a277ada1f6e4f235e46a6be40bace37852ee
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2025-06-24 20:59:06 +0600

    Test

--- Submissions/Others/Codes/rough.cpp
+++ Submissions/Others/Codes/rough.cpp
@@ -1,32 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void solve() {
-  int n; cin >> n;
-  int a[n + 1];
-  for (int i = 1; i <= n; i++) {
-    cin >> a[i];
-  }
-
-  int x = a[1];
-  for (int i = 2; i <= n; i++) {
-    if (x > a[i]) {
-      cout << "NO\n";
-      return;
-    }
-    x = a[i] - x;
-  }
-  cout << "YES\n";
-}
+typedef long long ll;
 
 int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  int t = 1; cin >> t;
-  while(t--) {
-    solve();
-  }
+  int n; cin >> n;
+  cout << n << ' ';
+  cout << "hello new\n";
 
   return 0;
 }
